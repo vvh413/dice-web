eb564d628e4ec795575e015d766a6e490f8f673b
vvh413 <nnenekit@gmail.com>
2022-11-06 03:27:11 +0300

    limit x to 2^27 + raise version to 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use actix_web::middleware::Logger;
 use actix_web::HttpResponse;
-use actix_web::{get, web, App, HttpServer, Result};
+use actix_web::{get, web, App, HttpServer};
+use derive_more::{Display, Error};
 use env_logger::Env;
 use rand::thread_rng;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 
+const MAX_DICE: u32 = 2 << 27;
+
 #[derive(Deserialize)]
 struct Roll {
-    x: u64,
+    x: u32,
     y: u64,
 }
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "roll error: {}", message)]
+struct RollError {
+    message: &'static str,
+}
+impl actix_web::error::ResponseError for RollError {}
 
 #[derive(Serialize)]
 struct Dice {
@@ -21,25 +31,30 @@ struct Dice {
 }
 
 impl Dice {
-    fn roll(x: u64, y: u64) -> Self {
+    fn roll(x: u32, y: u64) -> Result<Self, RollError> {
+        if x > MAX_DICE {
+            return Err(RollError {
+                message: "too many dice",
+            });
+        };
         let values: Vec<u64> = (0..x).map(|_| thread_rng().gen_range(1..=y)).collect();
-        Dice {
+        Ok(Dice {
             values: values.clone(),
             total: values.iter().sum(),
             min: *values.iter().min().unwrap(),
             max: *values.iter().max().unwrap(),
-        }
+        })
     }
 }
 
 #[get("/{x}d{y}")]
-async fn dice(roll: web::Path<Roll>) -> Result<web::Json<Dice>> {
-    Ok(web::Json(Dice::roll(roll.x, roll.y)))
+async fn dice(roll: web::Path<Roll>) -> Result<web::Json<Dice>, RollError> {
+    Ok(web::Json(Dice::roll(roll.x, roll.y)?))
 }
 
 #[get("/")]
-async fn default() -> Result<web::Json<Dice>> {
-    Ok(web::Json(Dice::roll(1, 100)))
+async fn default() -> Result<web::Json<Dice>, RollError> {
+    Ok(web::Json(Dice::roll(1, 100)?))
 }
 
 #[actix_web::main]
