333e70fd4c54288a2c1243e97b23e6819a89a801
vvh413 <nnenekit@gmail.com>
2022-11-06 18:42:44 +0300

    add seeded dice + use ChaCha20 rng

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use actix_web::middleware::Logger;
-use actix_web::HttpResponse;
 use actix_web::{get, web, App, HttpServer};
+use actix_web::{HttpResponse, ResponseError};
 use derive_more::{Display, Error};
 use env_logger::Env;
-use rand::thread_rng;
+use hex::FromHexError;
 use rand::Rng;
+use rand::SeedableRng;
+use rand_chacha::ChaCha20Rng;
 use serde::{Deserialize, Serialize};
 
 const MAX_DICE: u32 = 2 << 25;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Display)]
+#[display(fmt = "{}d{}", x, y)]
 struct Roll {
     x: u32,
     y: u64,
+}
+
+#[derive(Deserialize)]
+struct SeededRoll {
+    x: u32,
+    y: u64,
+    seed: String,
 }
 
 #[derive(Debug, Display, Error)]
@@ -20,7 +30,14 @@ struct Roll {
 struct RollError {
     message: &'static str,
 }
-impl actix_web::error::ResponseError for RollError {}
+impl ResponseError for RollError {}
+impl From<FromHexError> for RollError {
+    fn from(_: FromHexError) -> Self {
+        RollError {
+            message: "invalid hex value for seed",
+        }
+    }
+}
 
 #[derive(Serialize)]
 struct Dice {
@@ -31,13 +48,14 @@ struct Dice {
 }
 
 impl Dice {
-    fn roll(x: u32, y: u64) -> Result<Self, RollError> {
+    fn roll(x: u32, y: u64, seed: [u8; 32]) -> Result<Self, RollError> {
         if x > MAX_DICE {
             return Err(RollError {
                 message: "too many dice",
             });
         };
-        let values: Vec<u64> = (0..x).map(|_| thread_rng().gen_range(1..=y)).collect();
+        let mut rng = ChaCha20Rng::from_seed(seed);
+        let values: Vec<u64> = (0..x).map(|_| rng.gen_range(1..=y)).collect();
         Ok(Dice {
             values: values.clone(),
             total: values.iter().sum(),
@@ -47,14 +65,27 @@ impl Dice {
     }
 }
 
+#[get("/{x}d{y}:{seed}")]
+async fn seeded_dice(roll: web::Path<SeededRoll>) -> Result<web::Json<Dice>, RollError> {
+    let mut seed = [0u8; 32];
+    hex::decode_to_slice(roll.seed.clone(), &mut seed)?;
+    Ok(web::Json(Dice::roll(roll.x, roll.y, seed)?))
+}
+
 #[get("/{x}d{y}")]
-async fn dice(roll: web::Path<Roll>) -> Result<web::Json<Dice>, RollError> {
-    Ok(web::Json(Dice::roll(roll.x, roll.y)?))
+async fn dice(roll: web::Path<Roll>) -> HttpResponse {
+    let seed = hex::encode(ChaCha20Rng::from_entropy().get_seed());
+    HttpResponse::Found()
+        .append_header(("location", format!("/{}:{}", roll, seed)))
+        .finish()
 }
 
 #[get("/")]
-async fn default() -> Result<web::Json<Dice>, RollError> {
-    Ok(web::Json(Dice::roll(1, 100)?))
+async fn default() -> HttpResponse {
+    let seed = hex::encode(ChaCha20Rng::from_entropy().get_seed());
+    HttpResponse::Found()
+        .append_header(("location", format!("/1d100:{}", seed)))
+        .finish()
 }
 
 #[actix_web::main]
@@ -63,6 +94,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
+            .service(seeded_dice)
             .service(dice)
             .service(default)
             .route("/health", web::get().to(HttpResponse::Ok))
